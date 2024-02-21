d1f9b0277f41625f3457bbb9a2ee055b44b1ed6f
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-02-21 16:48:23 +0000

    Add SqliteConnection

---
+++ api/src/src/SqliteConnection.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App;
+
+/**
+ * A connection to a SQLite database
+ * @author Kieran
+ */
+class SqliteConnection implements Connection
+{
+    private \PDO $pdo;
+
+    /**
+     * Connect to a SQLite database
+     * @param string $path The path to the database file, can be ":memory:" for an in-memory database
+     */
+    public function __construct(string $path)
+    {
+        $this->pdo = new \PDO("sqlite:$path");
+        $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+    }
+
+
+    // @phpstan-ignore-next-line - Return type depends on the query. Individual methods should specify the return type
+    public function query(string $query, array $params = []): array
+    {
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($params);
+        return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+    }
+
+    public function execute(string $query, array $params = []): int
+    {
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($params);
+        return $stmt->rowCount();
+    }
+
+    public function lastInsertId(): string
+    {
+        $id = $this->pdo->lastInsertId();
+        if ($id === false) {
+            throw new \LogicException('Could not get last insert ID, was anything inserted?');
+        }
+        return $id;
+    }
+}

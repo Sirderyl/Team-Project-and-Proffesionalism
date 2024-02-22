69c6b0c57ec978d9f52d9cc60a84337ec8fe766e
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-02-22 14:39:25 +0000

    Add first revision of the schema

---
+++ api/schema.sql
@@ -0,0 +1,36 @@
+-- Schema for the database
+-- Expected to be run on an empty database
+
+CREATE TABLE user (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    email TEXT NOT NULL UNIQUE,
+    password_hash TEXT NOT NULL,
+    profile_picture BLOB
+);
+
+CREATE TABLE organization (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    manager_id INTEGER NOT NULL REFERENCES user(id)
+);
+
+CREATE TABLE activity (
+    id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    name TEXT NOT NULL,
+    short_description TEXT NOT NULL,
+    long_description TEXT NOT NULL,
+    preview_picture BLOB,
+    -- TODO: We need a way to support recurring events
+    start_time DATETIME NOT NULL,
+    end_time DATETIME NOT NULL,
+    needed_volunteers INTEGER NOT NULL
+);
+
+CREATE TABLE user_activity (
+    user_id INTEGER NOT NULL REFERENCES user(id),
+    activity_id INTEGER NOT NULL REFERENCES activity(id),
+    rating INTEGER,
+    -- TODO: Once recurring events are supported, we need to add a date field and add it to the primary key
+    PRIMARY KEY (user_id, activity_id)
+);

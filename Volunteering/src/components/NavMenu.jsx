099451207bda93dc9aec33bb0a9932af6945f926
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-02-23 15:12:48 +0000

    Use a flexbox for nav items

--- Volunteering/src/components/NavMenu.jsx
+++ Volunteering/src/components/NavMenu.jsx
@@ -3,11 +3,12 @@ import { Link } from "react-router-dom";
 
 /**
  * Navigation menu
+ * @author Kieran
  */
 export default function NavMenu({ routes }) {
     return (
         <nav>
-            <ul>
+            <ul className="flex flex-col md:flex-row justify-evenly">
             {routes.map((route, index) => (
                 <li key={index}><Link to={route.path}>{route.name}</Link></li>
             ))}

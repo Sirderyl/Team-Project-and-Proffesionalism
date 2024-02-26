93fbc472f51d99b11e24bcdeee52788e668465fb
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-02-26 16:56:32 +0000

    Add transaction functions

--- api/src/src/Database/Database.php
+++ api/src/src/Database/Database.php
@@ -8,15 +8,32 @@ namespace App\Database;
  */
 class Database implements DatabaseInterface
 {
+    private readonly ConnectionInterface $connection;
     private readonly UsersDatabaseInterface $users;
 
     public function __construct(ConnectionInterface $connection)
     {
+        $this->connection = $connection;
         $this->users = new UsersDatabase($connection);
     }
 
     public function users(): UsersDatabaseInterface
     {
         return $this->users;
+    }
+
+    public function beginTransaction(): void
+    {
+        $this->connection->execute('BEGIN TRANSACTION');
+    }
+
+    public function commit(): void
+    {
+        $this->connection->execute('COMMIT');
+    }
+
+    public function rollback(): void
+    {
+        $this->connection->execute('ROLLBACK');
     }
 }
--- api/src/src/Database/DatabaseInterface.php
+++ api/src/src/Database/DatabaseInterface.php
@@ -9,4 +9,19 @@ namespace App\Database;
 interface DatabaseInterface
 {
     public function users(): UsersDatabaseInterface;
+
+    /**
+     * Begin a transaction. Should be called before any setters are called
+     */
+    public function beginTransaction(): void;
+
+    /**
+     * Commit a transaction. Should be called after all setters have been called
+     */
+    public function commit(): void;
+
+    /**
+     * Rollback a transaction. Should be called in the catch block of a try-catch statement
+     */
+    public function rollback(): void;
 }

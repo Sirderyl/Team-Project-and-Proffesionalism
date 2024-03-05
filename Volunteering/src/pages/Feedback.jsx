03300635c734a3d2f68aad8826e3090bfc87c6a7
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-03-05 14:37:13 +0000

    Added useState variable to store captured rating

--- Volunteering/src/pages/Feedback.jsx
+++ Volunteering/src/pages/Feedback.jsx
@@ -1,13 +1,15 @@
 import { Rating } from "@mui/material"
 import placeholder from '../assets/placeholder.jpg';
+import {useState} from 'react';
 
 function Feedback() {
+    const [activityRating, setActivityRating] = useState();
     return (
         <div>
             <h1 className="text-3xl font-bold mb-3 ml-5">Volunteering Feedback</h1>
             { <img className="w-40 h-40 ml-5" src={placeholder}/>}
             <p className="text-lg mt-6 ml-5">Rate your experience Clearing Tables with Middlesbrough Soup Kitchen:</p>       
-            <p className="ml-5 mt-6">Clearing Tables</p><Rating className="ml-5"></Rating>
+            <p className="ml-5 mt-6">Clearing Tables</p><Rating className="ml-5" value={activityRating} onChange={e => setActivityRating(e.target.value)}></Rating>
             <p className="text-lg mt-6 ml-5">Rate your experience volunteering with the following people:</p>  
             <div className="flex flex-col ml-5 mt-6">  
                     <p>John</p><Rating></Rating>

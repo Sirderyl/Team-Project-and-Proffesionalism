1cc56e838a89e7d4decfe3f3bd9eb5199c9a036d
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-03-12 13:55:53 +0000

    Test register endpoint

---
+++ api/tests/RegisterTest.php
@@ -0,0 +1,51 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use App\Database;
+use App\Register;
+use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertNotEquals;
+
+class RegisterTest extends TestCase {
+    private const BODY = [
+        'name' => 'Bob',
+        'email' => 'bob@example.com',
+        'password' => 'Pa55w0rd!',
+        'phone' => '123-456-7890'
+    ];
+
+    private Database\DatabaseInterface $database;
+    private Register $register;
+
+    protected function setUp(): void {
+        $connection = new App\Database\SqliteConnection(':memory:');
+        $this->database = new App\Database\Database($connection);
+        $this->register = new App\Register($this->database);
+    }
+
+    public function testParseBody(): void {
+        $encoded = json_encode(self::BODY);
+        // Should never happen, check to make PHPStan happy
+        if ($encoded === false) throw new Exception('Failed to encode JSON');
+        $parsed = $this->register->parseBody($encoded);
+
+        assertEquals(self::BODY, $parsed);
+    }
+
+    public function testExecute(): void {
+        $result = $this->register->execute(self::BODY);
+
+        $user = $this->database->users()->get(self::BODY['email']);
+        $this->assertEquals(App\Token::verify($result['token']), $user->userId);
+        $this->assertEquals(self::BODY['name'], $user->userName);
+        $this->assertEquals(self::BODY['phone'], $user->phoneNumber);
+        $this->assertEquals(self::BODY['email'], $user->email);
+        $this->assertTrue(password_verify(self::BODY['password'], $user->passwordHash));
+    }
+
+    public function testFailIfCredentialsTaken(): void {
+        $this->register->execute(self::BODY);
+        $this->expectException(PDOException::class);
+        $this->register->execute(self::BODY);
+    }
+}

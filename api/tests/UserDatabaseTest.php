14d8c66bf2e0df300e46ef59bb7d61d826952858
kieranknowles1 <kieranknowles11@hotmail.co.uk>
2024-03-15 12:29:18 +0000

    Test profile pic function

--- api/tests/UserDatabaseTest.php
+++ api/tests/UserDatabaseTest.php
@@ -24,4 +24,24 @@ final class UserDatabaseTest extends TestCase
 
         $this->assertEquals($user, $output);
     }
+
+    public function testProfilePicture(): void
+    {
+        [$user] = Debug\DebugUser::createDummyUser($this->faker);
+        $this->database->users()->create($user);
+
+        // Image faker is deprecated, so just use a random string
+        $picture = $this->faker->password(512, 512);
+        $this->database->users()->setProfilePicture($user->userId, $picture);
+
+        $output = $this->database->users()->getProfilePicture($user->userId);
+
+        $this->assertEquals($picture, $output);
+    }
+
+    public function testPictureNotFoundIfNoUser(): void
+    {
+        $this->expectException(\App\Database\NotFoundException::class);
+        $this->database->users()->getProfilePicture(1234);
+    }
 }

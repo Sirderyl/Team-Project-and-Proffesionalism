fe2da3dd150dfa9b820fd1dd07c310af55e9e9a8
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-03-22 15:42:59 +0000

    Made Feedback form use API to retrieve activity name, image and organization name

--- Volunteering/src/pages/Feedback.jsx
+++ Volunteering/src/pages/Feedback.jsx
@@ -1,15 +1,38 @@
 import { Rating } from "@mui/material"
-import placeholder from '../assets/placeholder.jpg';
-import {useState} from 'react';
+import { apiRoot } from '../settings'
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function Feedback() {
     const [activityRating, setActivityRating] = useState();
+    const [activity, setActivity] = useState();
+
+    useEffect(() => {
+        const fetchActivity = async () => {
+            try {
+                const response = await axios.get('https://w21017158.nuwebspace.co.uk/api/activity/1');
+                setActivity(response.data);
+            } catch (error) {
+                console.error('Error fetching activity:', error);
+            }
+        };
+
+        fetchActivity();
+    }, []);
+
     return (
         <div>
+            
             <h1 className="text-3xl font-bold mb-3 ml-5">Volunteering Feedback</h1>
-            { <img className="w-40 h-40 ml-5" src={placeholder}/>}
-            <p className="text-lg mt-6 ml-5">Rate your experience Clearing Tables with Middlesbrough Soup Kitchen:</p>       
-            <p className="ml-5 mt-6">Clearing Tables</p><Rating className="ml-5" value={activityRating} onChange={e => setActivityRating(e.target.value)}></Rating>
+            { <img className="w-40 h-40 ml-5" src={`${apiRoot}/activity/1/previewimage`}/>}
+            {activity && (
+            <p className="text-lg mt-6 ml-5">Rate your experience doing the activity, {activity.name} with the organization, {activity.organization.name}:</p>       
+            )}
+            {activity && (
+            <p className="ml-5 mt-6">{activity.name}</p>
+            )}
+            <Rating className="ml-5" value={activityRating} onChange={e => setActivityRating(e.target.value)}></Rating>
+            
             <p className="text-lg mt-6 ml-5">Rate your experience volunteering with the following people:</p>  
             <div className="flex flex-col ml-5 mt-6">  
                     <p>John</p><Rating></Rating>
@@ -20,6 +43,7 @@ function Feedback() {
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5">Submit Feedback</button>
                 <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 ml-3">Skip</button>
             </div>
+            
         </div>
     )
 }

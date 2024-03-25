34268d5c0f405f1fa3a77ccf528e45f33bf512ed
Roy2105 <118737632+Roy2105@users.noreply.github.com>
2024-03-25 07:08:55 +0000

    added notifications to navbar if user is logged in

--- Volunteering/src/components/NavMenu.jsx
+++ Volunteering/src/components/NavMenu.jsx
@@ -1,5 +1,6 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import NotificationHandler from './NotificationHandler';
 
 /**
  * Navigation menu
@@ -16,7 +17,12 @@ export default function NavMenu({
             {routes.map((route, index) => (
                 <li key={index}><Link to={route.path}>{route.name}</Link></li>
             ))}
-            {isLoggedIn && <li><button onClick={handleLogout}>Log Out</button></li>}
+            {isLoggedIn && (
+            <>
+                <li><button onClick={handleLogout}>Log Out</button></li>
+                <li><NotificationHandler /></li>
+            </>
+            )}
             </ul>
         </nav>
     )

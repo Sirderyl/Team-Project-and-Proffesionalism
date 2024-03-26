34216c64b345168c58d6aa1aa162c09be5c8a691
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-03-26 06:12:38 +0000

    Call API to set activity rating

--- Volunteering/src/pages/Feedback.jsx
+++ Volunteering/src/pages/Feedback.jsx
@@ -20,6 +20,22 @@ function Feedback() {
         fetchActivity();
     }, []);
 
+
+    const handleSubmitFeedback = async () => {
+        console.log(activityRating);
+        try {
+            await axios.post('https://w21017158.nuwebspace.co.uk/api/userSchedule/rating', null, {
+                params: {
+                    id: 1,
+                    rating: activityRating
+                }
+            });
+        } catch (error) {
+            console.error('Error submitting feedback:', error);
+        }
+    };
+    
+
     return (
         <div>
             
@@ -31,7 +47,7 @@ function Feedback() {
             {activity && (
             <p className="ml-5 mt-6">{activity.name}</p>
             )}
-            <Rating className="ml-5" value={activityRating} onChange={e => setActivityRating(e.target.value)}></Rating>
+            <Rating className="ml-5" value={activityRating ? activityRating : null} onChange={e => setActivityRating(parseInt(e.target.value))}></Rating>
             
             <p className="text-lg mt-6 ml-5">Rate your experience volunteering with the following people:</p>  
             <div className="flex flex-col ml-5 mt-6">  
@@ -40,8 +56,8 @@ function Feedback() {
                     <p>Alex</p><Rating></Rating>
                 </div>
             <div className="flex flex-row ml-5">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5">Submit Feedback</button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 ml-3">Skip</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" onClick={handleSubmitFeedback}>Submit Feedback</button>
+                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 ml-3">Back</button>
             </div>
             
         </div>

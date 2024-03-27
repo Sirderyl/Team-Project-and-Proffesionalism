f9cd103a2e7f55d44237ece503b1149b8150cae7
Andreas Meghoo <andyjmeghoo@gmail.com>
2024-03-27 20:36:28 +0000

    Add dropdown for selecting past Activity. and logic for rating that selected activity.

--- Volunteering/src/pages/Feedback.jsx
+++ Volunteering/src/pages/Feedback.jsx
@@ -1,34 +1,58 @@
 import { Rating } from "@mui/material"
 import { apiRoot } from '../settings'
-import {useState, useEffect} from 'react';
-import {Link} from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 import PropTypes from 'prop-types'
 
-export default function Feedback({user}) {
+export default function Feedback({ user }) {
     const [activityRating, setActivityRating] = useState();
-    const [activity, setActivity] = useState();
+    const [selectedActivity, setSelectedActivity] = useState();
+    const [pastActivities, setPastActivities] = useState([]);
+    const currentDate = new Date();
+    const startDate = new Date();
+    startDate.setDate(currentDate.getDate() - 7);
+
+    const currentDateStr = currentDate.toISOString().split('T')[0];
+    const startDateStr = startDate.toISOString().split('T')[0];
 
     useEffect(() => {
-        const fetchActivity = async () => {
+        const fetchPastActivities = async () => {
             try {
-                const response = await axios.get('https://w21017158.nuwebspace.co.uk/api/activity/1');
-                setActivity(response.data);
+                const response = await fetch(`https://w21017158.nuwebspace.co.uk/api/userSchedule/${user.userId}?start=${startDateStr}&end=${currentDateStr}`);
+                if (!response.ok) {
+                    throw new Error('Failed to fetch tasks');
+                }
+                const data = await response.json();
+                setPastActivities(data);
             } catch (error) {
-                console.error('Error fetching activity:', error);
+                console.error('Error fetching tasks:', error);
             }
         };
 
-        fetchActivity();
-    }, []);
+        fetchPastActivities();
+    }, [user, currentDateStr, startDateStr]);
+
+    const handleSelectActivity = (selectedActivityId) => {
+        
+        const strSelectedActivityId = parseInt(selectedActivityId);
+        console.log(strSelectedActivityId);
+        const selectedActivity = pastActivities.find(activity => activity.activity.id === strSelectedActivityId);
+        if (selectedActivity) {
+            setSelectedActivity(selectedActivity);
+        } else {
+            console.log("Activity not found with ID:", selectedActivityId);
+        }
+    }
+
+
 
 
     const handleSubmitFeedback = async () => {
-        console.log(activityRating);
         try {
             await axios.post('https://w21017158.nuwebspace.co.uk/api/userSchedule/rating', null, {
                 params: {
-                    id: 1,
+                    id: selectedActivity.activity.id,
                     rating: activityRating
                 }
             });
@@ -36,8 +60,8 @@ export default function Feedback({user}) {
             console.error('Error submitting feedback:', error);
         }
     };
-    
-    if(user.isManager) {
+
+    if (user.isManager) {
         return (
             <div>
                 <h1 className="text-3xl font-bold mb-3 ml-5">Managers are not allowed to rate activities</h1>
@@ -49,30 +73,41 @@ export default function Feedback({user}) {
     }
     return (
         <div>
-            
+
             <h1 className="text-3xl font-bold mb-3 ml-5">Volunteering Feedback</h1>
-            { <img className="w-40 h-40 ml-5" src={`${apiRoot}/activity/1/previewimage`}/>}
-            {activity && (
-            <p className="text-lg mt-6 ml-5">Rate your experience doing the activity, {activity.name} with the organization, {activity.organization.name}:</p>       
-            )}
-            {activity && (
-            <p className="ml-5 mt-6">{activity.name}</p>
-            )}
-            <Rating className="ml-5" value={activityRating ? activityRating : null} onChange={e => setActivityRating(parseInt(e.target.value))}></Rating>
-            
-            <p className="text-lg mt-6 ml-5">Rate your experience volunteering with the following people:</p>  
-            <div className="flex flex-col ml-5 mt-6">  
-                    <p>John</p><Rating></Rating>
-                    <p>Emma</p><Rating></Rating>
-                    <p>Alex</p><Rating></Rating>
-                </div>
-            <div className="flex flex-row ml-5">
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" onClick={handleSubmitFeedback}>Submit Feedback</button>
-                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5 ml-3">Back</button>
-            </div>
-            
+            <form className="max-w-sm ml-5">
+                <label htmlFor="assignedTasks" className=" ml-5 block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select an activity you have completed: </label>
+                <select id="assignedTasks" className="ml-5 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" onChange={(e) => handleSelectActivity(e.target.value)}>
+                    {pastActivities.map((activity, index) => (
+                        <option key={index + 1} value={activity.activity.id}>
+                            {activity.activity.name} - {new Date(activity.start.date).toLocaleDateString('en-US',
+                                { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: '2-digit', minute: '2-digit' })}
+                        </option>
+                    ))}
+                </select>
+            </form>
+
+            {selectedActivity && selectedActivity.activity && (
+                <>
+                    {<img className="w-40 mt-6 h-40 ml-5" src={`${apiRoot}/activity/${selectedActivity.activity.id}/previewimage`} />}
+                    <p className="text-lg mt-6 ml-5">Rate your experience doing the activity, {selectedActivity.activity.name}.</p>
+                    <p className="ml-5 mt-6">{selectedActivity.activity.name}</p>
+                    <Rating className="ml-5" value={activityRating ? activityRating : null} onChange={e => setActivityRating(parseInt(e.target.value))}></Rating>
+
+                    <p className="text-lg mt-6 ml-5">Rate your experience volunteering with the following people:</p>
+                    <div className="flex flex-col ml-5 mt-6">
+                        <p>John</p><Rating></Rating>
+                        <p>Emma</p><Rating></Rating>
+                        <p>Alex</p><Rating></Rating>
+                    </div>
+                        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5" onClick={handleSubmitFeedback}>Submit Feedback</button>
+                    </>
+                )}
+                
         </div>
     )
 }
 
-Feedback.propTypes = {user: PropTypes.object.isRequired}
+Feedback.propTypes = {
+    user: PropTypes.object.isRequired
+}

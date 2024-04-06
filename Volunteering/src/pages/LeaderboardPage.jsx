e7ab6b2f6e63eeb582836dcec6afc6ec80fc9899
Roy2105 <118737632+Roy2105@users.noreply.github.com>
2024-04-06 14:37:04 +0100

    Update leaderboard page formatting for consistency with other pages

--- Volunteering/src/pages/LeaderboardPage.jsx
+++ Volunteering/src/pages/LeaderboardPage.jsx
@@ -2,8 +2,8 @@ import Leaderboard from "../components/Leaderboard"
 
 export default function LeaderboardPage() {
     return (
-        <main>
-            <h1 className="text-3xl text-blue-700 font-bold mb-3 ml-5">Leaderboard</h1>
+        <main className="max-w-4xl mx-auto mt-8">
+            <h1 className="text-3xl text-blue-700 font-bold mb-4">Leaderboard</h1>
             <Leaderboard/>
         </main>
     )

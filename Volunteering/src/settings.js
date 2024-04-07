a7d52fe33e407836893c6156b653691c18281405
Roy2105 <118737632+Roy2105@users.noreply.github.com>
2024-04-07 19:43:36 +0100

    Revert api root change

--- Volunteering/src/settings.js
+++ Volunteering/src/settings.js
@@ -5,4 +5,4 @@
 // Use an external API, unless we're running on localhost
 const isLocalHost = window.location.hostname === 'localhost'
 
-export const apiRoot = isLocalHost ? 'https://w20022835.nuwebspace.co.uk/api' : '/api'
+export const apiRoot = isLocalHost ? 'https://w20013000.nuwebspace.co.uk/api' : '/api'
